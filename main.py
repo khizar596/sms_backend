269b86faed5ed11d7e68bd46766274331e474d47
Sardar Muhammad Khizar Rafique <72907586+khizar596@users.noreply.github.com>
2023-03-03 11:21:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ origins = [ 'https://ilh5jagznl.execute-api.us-east-2.amazonaws.com:8000/']
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
